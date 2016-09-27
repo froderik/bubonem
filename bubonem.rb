6da227909197a4ea43fd730814968ad1a8685e77
Fredrik Rubensson <fredrik@eldfluga.se>
2016-09-27 08:01:15 +0200

    forecast piping

--- bubonem.rb
+++ bubonem.rb
@@ -17,13 +17,27 @@ class Bubonem < Sinatra::Base
   end
 
   get '/weather_forecast' do
-    "weather!"
+    present_weather_forecast
   end
 
+
+  # bus information
+  
   def present_stop_information stop_id
     response = RestClient.get "http://sl.se/api/sv/RealTime/GetDepartures/#{stop_id}"
     the_data = JSON.parse(response)["data"]["BusGroups"].first
     haml :stop_information, locals: { data: the_data }
   end
+
+
+  # weather forecast
+
+  def present_weather_forecast
+    response = RestClient.get 'http://opendata-download-metfcst.smhi.se/api/category/pmp2g/version/2/geotype/point/lon/17.96/lat/59.44/data.json'
+    the_data = JSON.parse(response)
+    haml :forecast, locals: { data: the_data }
+  end
   
 end
+
+
