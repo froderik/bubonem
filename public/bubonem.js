724ab89091794aaff6b592b62b80c221c2acc8f8
Fredrik Rubensson <fredrik@eldfluga.se>
2016-10-01 22:31:47 +0200

    indentation

--- public/bubonem.js
+++ public/bubonem.js
@@ -2,14 +2,14 @@
 
 var content_magic = function(name) {
     $.get('/' + name, function(response) {
-	$('.' + name).html(response);
+        $('.' + name).html(response);
     });
 };
 
 $(function() {
     let parts = ['edsbergsskolan', 'axroad', 'weather_forecast'];
     for(let one_part of parts) {
-	content_magic(one_part);
+        content_magic(one_part);
     };
 });
 
