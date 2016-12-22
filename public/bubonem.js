ba8865226683629b93d4cccae1e24f9462762688
Fredrik Rubensson <fredrik@eldfluga.se>
2016-12-22 09:47:37 +0100

    trying out a more dynamic way of loading things - wip

--- public/bubonem.js
+++ public/bubonem.js
@@ -1,23 +1,42 @@
 
-var wave_the_staff = function(name) {
+var old_wave_the_staff = function(name) {
     return function() {
         var timeout_minutes = $('.' + name).attr('data-timeout');
         var timeout_millis = parseInt( timeout_minutes ) * 60 * 1000;
-        content_magic(name);
-        window.setTimeout( wave_the_staff(name), timeout_millis );
+        old_content_magic(name);
+        window.setTimeout( old_wave_the_staff(name), timeout_millis );
     };
 }
 
-var content_magic = function(name) {
+var wave_the_staff = function() {
+    console.log("waving the staff");
+    return function() {
+        var timeout_minutes = $(this).attr('data-timeout');
+        var timeout_millis = parseInt( timeout_minutes ) * 60 * 1000;
+        content_magic(this);
+        window.setTimeout( wave_the_staff(), timeout_millis );
+    };
+}
+
+var old_content_magic = function(name) {
     $.get('/' + name, function(response) {
         $('.' + name).html(response);
     });
 };
 
+var content_magic = function(widget) {
+    var url = $(widget).attr('data-url');
+    $.get(url, function(response) {
+        $(widget).html(response);
+    });
+};
+
 $(function() {
-    let parts = ['edsbergsskolan', 'axroad', 'weather_forecast', 'current_time', 'sun'];
+    let parts = ['edsbergsskolan', 'axroad', 'weather_forecast', 'current_time'];
     for(let one_part of parts) {
-        wave_the_staff(one_part)();
+        old_wave_the_staff(one_part)();
     };
+
+    $(".widget").each(wave_the_staff(this));
 });
 
