217f4fadc0999984c4e20881c5db63b1f349b014
Fredrik Rubensson <fredrik@eldfluga.se>
2016-12-22 16:54:55 +0100

    uses the new way of realoading widgets

--- public/bubonem.js
+++ public/bubonem.js
@@ -1,28 +1,12 @@
 
-var old_wave_the_staff = function(name) {
+var wave_the_staff = function(widget) {
     return function() {
-        var timeout_minutes = $('.' + name).attr('data-timeout');
+        var timeout_minutes = $(widget).attr("data-timeout");
         var timeout_millis = parseInt( timeout_minutes ) * 60 * 1000;
-        old_content_magic(name);
-        window.setTimeout( old_wave_the_staff(name), timeout_millis );
+        content_magic(widget);
+        window.setTimeout( wave_the_staff(widget), timeout_millis );
     };
 }
-
-var wave_the_staff = function() {
-    console.log("waving the staff");
-    return function() {
-        var timeout_minutes = $(this).attr('data-timeout');
-        var timeout_millis = parseInt( timeout_minutes ) * 60 * 1000;
-        content_magic(this);
-        window.setTimeout( wave_the_staff(), timeout_millis );
-    };
-}
-
-var old_content_magic = function(name) {
-    $.get('/' + name, function(response) {
-        $('.' + name).html(response);
-    });
-};
 
 var content_magic = function(widget) {
     var url = $(widget).attr('data-url');
@@ -32,11 +16,7 @@ var content_magic = function(widget) {
 };
 
 $(function() {
-    let parts = ['edsbergsskolan', 'axroad', 'weather_forecast', 'current_time'];
-    for(let one_part of parts) {
-        old_wave_the_staff(one_part)();
-    };
-
-    $(".widget").each(wave_the_staff(this));
+    // the function needs to be inside a funcition in order to get hold of this
+    $(".widget").each(function(index) { wave_the_staff(this)() });
 });
 
