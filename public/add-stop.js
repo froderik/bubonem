9475ee997934f9b12f68b8bf88ff83305bf15a03
Fredrik Rubensson <fredrik@eldfluga.se>
2017-02-04 22:03:28 +0100

    some scripting and an overview of the whole setup process

--- public/add-stop.js
+++ public/add-stop.js
@@ -1,4 +1,12 @@
 
+
+var search_for_stop = function() {
+    var q = $(".stop-search").val();
+
+    console.log("yo " + q);
+};
+
+
 var show_stop_picker = function() {
     $(".stop-picker").show();
 };
@@ -10,4 +18,5 @@ var hide_stop_picker = function() {
 $(function() {
     $(".add-stop").click(show_stop_picker);
     $(".stop-picker .close").click(hide_stop_picker);
+    $(".stop-search").on("input", search_for_stop);
 });
