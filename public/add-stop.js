53beb02e4c5e1ffb59b57deec1615bb35b29e0a2
Fredrik Rubensson <fredrik@eldfluga.se>
2017-02-06 15:55:35 +0100

    can select a stop from the list

--- public/add-stop.js
+++ public/add-stop.js
@@ -1,11 +1,25 @@
 
+var select_stop = function( event ) {
+    var stop_name = $(this).text();
+    var stop_id = $(this).attr( "data-stop-id" );
+
+    var stop_markup = "<li data-stop-id=\"" + stop_id + "\">" + stop_name + "</li>";
+    
+    $("#selected-stops-list").append(stop_markup);
+    hide_stop_picker();
+};
+
+var show_search_result = function( data ) {
+    $("#stop-list").html( data );
+    $(".one-stop-to-select").click( select_stop );
+};
 
 var search_for_stop = function() {
     var q = $(".stop-search").val();
     if(q.length >= 3) {
-        $.get( "stations/" + q, function(data) {
-            $( "#stop-list" ).html( data );
-        });
+        $.get( "stations/" + q, show_search_result);
+    } else {
+        $( "#stop-list" ).html( "" );
     }
 };
 
