17ce6dba4646dfc229d948bdf0c2c481d70bfd54
Fredrik Rubensson <fredrik@eldfluga.se>
2017-02-24 18:34:19 +0100

    refactor for the future win

--- public/link-calculator.js
+++ public/link-calculator.js
@@ -1,7 +1,5 @@
 
-var calculate_link = function() {
-    var url = "/dash?";
-
+var grab_stop_information = function() {
     var stops_ary = []
     $(".selected-stop").each(function() {
         var stop_id = $(this).attr( "data-stop-id" );
@@ -10,8 +8,28 @@ var calculate_link = function() {
     });
 
     if( stops_ary.length > 0 ) {
-        url += "stops=" + stops_ary.join();
+        return "stops=" + stops_ary.join();
+    } else {
+        return "";
     }
+};
+
+var push_unless_empty = function(ary, part) {
+    if( part != "" ) {
+        ary.push(part);
+    }
+};
+
+var calculate_link = function() {
+    var url = "/dash?";
+
+    var params_parts = [];
+
+    push_unless_empty( grab_stop_information() );
+
+    var all_params = params_parts.join("&");
+
+    url += all_params;
 
     $(".my-link").text( url );
     $(".my-link").attr( "href", url );
