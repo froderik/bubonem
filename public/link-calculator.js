6f7988e4f719752cc65ae4c5719008937725fe3e
Fredrik Rubensson <fredrik@eldfluga.se>
2017-02-25 11:25:44 +0100

    adds form for coordinates

--- public/link-calculator.js
+++ public/link-calculator.js
@@ -38,6 +38,8 @@ var calculate_link = function() {
     push_unless_empty( params_parts, grab_stop_information() );
     push_unless_empty( params_parts, grab_map_information("mapw") );
     push_unless_empty( params_parts, grab_map_information("maph") );
+    push_unless_empty( params_parts, grab_map_information("lat") );
+    push_unless_empty( params_parts, grab_map_information("lon") );
 
     var all_params = params_parts.join("&");
 
@@ -48,5 +50,6 @@ var calculate_link = function() {
 };
 
 $(function() {
+    $(".link-source").keypress(calculate_link);
     $(".link-source").change(calculate_link);
 });
