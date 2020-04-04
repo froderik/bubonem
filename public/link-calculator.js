5ee0dd5346d7c89649e3483e76a71306a70b5e03
Fredrik Rubensson <fredrik@eldfluga.se>
2020-04-04 23:26:28 +0200

    link calculator is now free from jquery - var -> let

--- public/link-calculator.js
+++ public/link-calculator.js
@@ -1,12 +1,14 @@
 
-var grab_stop_information = function() {
-    var stops_ary = []
-    $(".selected-stop").each(function() {
-        var stop_id = $(this).attr( "data-stop-id" );
-        var stop_type = $(this).find(".stop-type-select").val();
-        var this_part = stop_id + ":" + stop_type;
+let grab_stop_information = () => {
+    let stops_ary = []
+
+    let stops_list = document.querySelectorAll(".selected-stop")
+    for(stop of stops_list) {
+        let stop_id = stop.attributes['data-stop-id'].value
+        let stop_type = stop.querySelector(".stop-type-select").value
+        let this_part = stop_id + ":" + stop_type;
         stops_ary.push(this_part);
-    });
+    }
 
     if( stops_ary.length > 0 ) {
         return "stops=" + stops_ary.join();
@@ -15,8 +17,8 @@ var grab_stop_information = function() {
     }
 };
 
-var grab_map_information = function(name) {
-    var value = $("#" + name).val();
+let grab_location_information = (name) => {
+    let value = document.querySelector("#" + name).value
 
     if( value === "" ) {
         return "";
@@ -25,37 +27,35 @@ var grab_map_information = function(name) {
 };
 
 
-var push_unless_empty = function(ary, part) {
+let push_unless_empty = (ary, part) => {
     if( part != "" ) {
         ary.push(part);
     }
 };
 
-var calculate_link = function() {
-    var url = "/dash?";
+let calculate_link = () => {
+    let url = "/dash?";
 
-    var params_parts = [];
+    let params_parts = [];
 
     push_unless_empty( params_parts, grab_stop_information() );
-    push_unless_empty( params_parts, grab_map_information("lat") );
-    push_unless_empty( params_parts, grab_map_information("lon") );
+    push_unless_empty( params_parts, grab_location_information("lat") );
+    push_unless_empty( params_parts, grab_location_information("lon") );
 
-    var all_params = params_parts.join("&");
+    let all_params = params_parts.join("&");
 
     url += all_params;
 
-    $(".my-link").text( url );
-    $(".my-link").attr( "href", url );
+    da_link = document.querySelector(".my-link")
+    da_link.innerHTML = url
+    da_link.href = url
 };
 
-var add_event = function(css_selector, event_name) {
+let add_event = (css_selector, event_name) => {
     for(node of document.querySelectorAll(css_selector)) {
         node.addEventListener(event_name, calculate_link);
     }
 }
-
-$(function() {
-})
 
 window.addEventListener('DOMContentLoaded', (e) => {
     add_event(".link-source", "keyup")
