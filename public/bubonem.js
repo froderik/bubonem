78f0d50babd00cecec97df35a0fd2f2f182229aa
Fredrik Rubensson <fredrik@eldfluga.se>
2020-04-05 17:53:12 +0200

    arrows and lets makes it into bubonem.js

--- public/bubonem.js
+++ public/bubonem.js
@@ -1,24 +1,24 @@
 
-var timeout_in_millis_for = function(widget) {
-    var timeout_minutes = $(widget).attr("data-timeout");
+let timeout_in_millis_for = (widget) => {
+    let timeout_minutes = $(widget).attr("data-timeout");
     return parseInt( timeout_minutes ) * 60 * 1000;
 };
 
-var wave_the_staff = function(widget) {
-    return function() {
+let wave_the_staff = (widget) => {
+    return () => {
         content_magic(widget);
         window.setTimeout( wave_the_staff(widget), timeout_in_millis_for(widget) );
     };
 }
 
-var content_magic = function(widget) {
-    var url = $(widget).attr('data-url');
+let content_magic = (widget) => {
+    let url = $(widget).attr('data-url');
     $.get(url)
-	.done( function(response) { $(widget).html(response) })
-	.fail( function() { $(widget).html("Anropet misslyckades") } );
+	.done( (response) => { $(widget).html(response) })
+	.fail( () => { $(widget).html("Anropet misslyckades") } );
 };
 
-$(function() {
+$(() => {
     // for every 'widget' - wave the staff to get content into it.
     // The function needs to be inside a function in order to get hold of this.
     $(".widget").each(function(index) { wave_the_staff(this)() });
